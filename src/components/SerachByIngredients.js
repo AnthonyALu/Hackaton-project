1c809a75e31ab3d7fa3c17fa2cc6797323d06a98
saman-zdf <samanfyz68@gmail.com>
2021-10-21 08:42:54 +0800

    Routing has been implemented, there three different pages

---
+++ src/components/SerachByIngredients.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchByIngredients = () => {
+  return (
+    <div className='container'>
+      <h1>This page is for serach by ingredients</h1>
+    </div>
+  );
+};
+
+export default SearchByIngredients;
