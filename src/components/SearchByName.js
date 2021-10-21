ba0fad297adb04f1b9e7fa13baac8a765a44eb58
eunbiggabi <eunbiggabi84@gmail.com>
2021-10-21 18:04:43 +1000

    search-by-name

--- src/components/SearchByName.js
+++ src/components/SearchByName.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import useFetch from "./useFetch.js"
 
 const SerachByName = () => {
+
+  const { data, loading, error } = useFetch("www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata");
+
+  if (loading) return <h1> LOADING...</h1>
+
+  if (error) console.log(error);
+
+
   return (
     <div className='container'>
-      <h1>This page is for serach by name</h1>
+      <p>{ data?.meals.strMeal}</p>
+      <div>{ data?.meals.strMealThumb }</div>
     </div>
   );
 };
